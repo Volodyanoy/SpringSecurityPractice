57442881b68f7ed06cad67d72be73ecd31101f4d
Volodyanoy <volodyanoy@gmail.com>
2025-08-01 17:03:14 +0700

    The authentication logic in AuthProviderImpl has been removed, and Spring Security is now doing this.

--- src/main/java/org/example/volodyanoy/SecurityApp/config/SecurityConfig.java
+++ src/main/java/org/example/volodyanoy/SecurityApp/config/SecurityConfig.java
@@ -1,26 +1,28 @@
 package org.example.volodyanoy.SecurityApp.config;
 
-import org.example.volodyanoy.SecurityApp.security.AuthProviderImpl;
+import org.example.volodyanoy.SecurityApp.services.PersonDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import java.util.List;
 
+
+@EnableWebSecurity
 @Configuration
 public class SecurityConfig {
+    private final PersonDetailsService personDetailsService;
 
-    private final AuthProviderImpl authProvider;
-
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    @Autowired
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
     }
 
     // основной бин для фильтра безопасности
@@ -37,11 +39,23 @@ public class SecurityConfig {
         return http.build();
     }
 
-    // регистрация кастомного AuthProvider
     @Bean
-    public AuthenticationManager authenticationManager() {
-        return new ProviderManager(List.of(authProvider));
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+
+        authManagerBuilder
+                .userDetailsService(personDetailsService)
+                .passwordEncoder(passwordEncoder());
+
+        return authManagerBuilder.build();
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
 
 
 }
